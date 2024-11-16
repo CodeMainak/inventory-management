2acd03c9b97388277ab3d1280cde46b494f4e3a9
Mainak Mondal <127854822+aimainakmondal@users.noreply.github.com>
2024-11-16 09:30:07 +0530

    added aws-s3 image url

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -2,6 +2,7 @@
 import { useAppDispatch, useAppSelector } from '@/app/redux'
 import { setIsDarkMode, setIsSidebarCollapsed } from '@/state'
 import { Bell, Menu, Moon, Settings, Sun } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -52,9 +53,9 @@ const Navbar = () => {
                     </div>
                     <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
                     <div className="flex items-center gap-3 cursor-pointer">
-                        <div className="w-9 h-9">
-                            image
-                        </div>
+                        <Image src="https://s3mckinventorymanagement.s3.ap-south-1.amazonaws.com/profile.png" alt='profile'
+                            width={50} height={50} className='rounded-full h-full object-cover'
+                        />
                         <span className="font-semibold">Mainak</span>
                     </div>
                 </div>
--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -2,6 +2,7 @@
 import { useAppDispatch, useAppSelector } from '@/app/redux';
 import { setIsSidebarCollapsed } from '@/state';
 import { Archive, CircleDollarSign, Clipboard, Layout, LucideIcon, Menu, SlidersHorizontal, User } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react'
@@ -44,7 +45,9 @@ const Sidebar = () => {
     return (
         <div className={sidebarClassNames}>
             <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${isSidebarCollapsed ? "px-5" : "px-8"}`}>
-                <div>Logo</div>
+                <Image src="https://s3mckinventorymanagement.s3.ap-south-1.amazonaws.com/logo.png" alt='logo'
+                    width={27} height={27} className='rounded w-8'
+                />
                 <h1 className={`${isSidebarCollapsed ? "hidden" : "block"} font-extrabold text-2xl`} >MKSTOCK</h1>
                 <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={toggleSidebar}>
                     <Menu className="w-4 h-4" />
